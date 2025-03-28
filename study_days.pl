dbfd152d8b75d98b7daae3ac09a0bcd91c33c458
Your Name <Zeyad1162005@gmail.com>
2025-03-28 22:21:00 +0200

    Study_Days Predicate

--- study_days.pl
+++ study_days.pl
@@ -1,1 +1,11 @@
 :- consult(’studentKB’).
+study_days(Slots, DayCount):-
+		setof(Day,allDays(Slots,Day),Days),
+		list_length(Days,ActualDays),
+		ActualDays=<DayCount.
+
+allDays([slot(Day,_,_)|_],Day).
+allDays([_|T],Day):- allDays(T,Day).
+		
+list_length([],0).
+list_length([_|T],N):- length(T,N1),N is N1+1.

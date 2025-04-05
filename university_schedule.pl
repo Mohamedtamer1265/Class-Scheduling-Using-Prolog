63b8462e4e183e8b0fcb1f5f6b3391903d0c5bef
Adham Jalaal <jalaaladham@gmail.com>
2025-04-05 08:16:14 +0200

    done parts a, b, c

--- university_schedule.pl
+++ university_schedule.pl
@@ -1,1 +1,6 @@
-:- consult(’studentKB’).
+:- consult('studentKB').
+:- consult('student_schedule')
+
+university_schedule(S):-
+    bagof(sched(student_id,Slots),student_schedule(student_id, Slots), S).
+    
